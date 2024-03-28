093aa6d4a5e176f42592565e3173b8278f92d5c1
apoorvaursal <apoorvaursal1111@gmail.com>
2024-03-28 15:09:42 +0530

    BasicProject Structure
    hi all ,
    its my project

---
+++ DemoblazeVersio1/src/main/java/DemoblazeVersio1/DemoblazeVersio1/App.java
@@ -0,0 +1,13 @@
+package DemoblazeVersio1.DemoblazeVersio1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
